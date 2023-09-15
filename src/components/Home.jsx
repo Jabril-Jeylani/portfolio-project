7a2a7e017f083c21c6dca3ac47c3a9e18f5f7d95
Jabril-Jeylani <jabrilmj@gmail.com>
2023-09-15 17:07:34 -0600

    work button working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
+import { useState } from "react";
 
 function Home() {
+	const [nav, setNav] = useState(false);
+	const handleClick = () => setNav(!nav);
 	return (
 		<div
 			name="home"
@@ -24,12 +28,19 @@ function Home() {
 					exciting field.
 				</p>
 				<div>
-					<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+				<Link onClick={handleClick}
+						to="work"
+						smooth={true}
+						duration={500}
+					>
+						<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
 						View Work
 						<span className="group-hover:rotate-90 duration-300">
 							<HiArrowNarrowRight className="ml-3" />
 						</span>
 					</button>
+					</Link>
+					
 				</div>
 			</div>
 		</div>
